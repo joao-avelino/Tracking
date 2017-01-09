525ae090ce09316981b2762a6172c2bb3c025571
Joao Avelino <joao.manuel.avelino@gmail.com>
2017-01-09 18:35:17 +0000

    Starting Bierman mex

--- matlab/_mex/BiermanKF_interface_mex.cpp
+++ matlab/_mex/BiermanKF_interface_mex.cpp
@@ -22,28 +22,109 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
         if (nlhs != 1)
             mexErrMsgTxt("New: One output expected.");
         // Return a handle to a new C++ instance
-	if(nrhs != 7)
+	if(nrhs != 2)
 	    mexErrMsgTxt("Wrong inputs");
 
-	MatrixXd stateTransitionModel;
-	MatrixXd observationModel;
-	MatrixXd processNoiseCovariance;
-	MatrixXd observationNoiseCov;
-	VectorXd initial_state;
+	
+	
 	MatrixXd initial_cov;
 
 
+
+	//Get the State Transition Matrix
 	double *stateTransitionPTR = mxGetPr(prhs[1]);
 	int stateTransM = mxGetM(prhs[1]);
 	int stateTransN = mxGetN(prhs[1]);
 
-	//Get the State Transition Matrix
+	MatrixXd stateTransitionModel(stateTransM, stateTransN);
 
-	for (int n = 0; n < stateTransN; n++)
+	
+	for (int m = 0; m < stateTransM; m++)
 	{
-
+		for (int n = 0; n < stateTransN; n++)
+		{
+			stateTransitionModel(m, n) = stateTransitionPTR[m + stateTransM*n];
+		}
 	}
 
+	//Get the observationModel
+	double *obsModelPTR = mxGetPr(prhs[2]);
+	int obsModelM = mxGetM(prhs[2]);
+	int obsModelN = mxGetN(prhs[2]);
+
+	MatrixXd observationModel(obsModelM, obsModelN);
+
+
+	for (int m = 0; m < obsModelM; m++)
+	{
+		for (int n = 0; n < obsModelN; n++)
+		{
+			observationModel(m, n) = obsModelPTR[m + obsModelM*n];
+		}
+	}
+
+	//Get the processNoiseCovariance
+	double *processNoisePTR = mxGetPr(prhs[3]);
+	int processNoiseM = mxGetM(prhs[3]);
+	int processNoiseN = mxGetN(prhs[3]);
+
+	MatrixXd processNoiseCovariance(processNoiseM, processNoiseN);
+
+
+	for (int m = 0; m < processNoiseM; m++)
+	{
+		for (int n = 0; n < processNoiseN; n++)
+		{
+			processNoiseCovariance(m, n) = processNoisePTR[m + processNoiseM*n];
+		}
+	}
+
+	//Get the observationNoiseCov
+	double *observationNoisePTR = mxGetPr(prhs[4]);
+	int observationNoiseM = mxGetM(prhs[4]);
+	int observationNoiseN = mxGetN(prhs[4]);
+
+	MatrixXd observationNoiseCov(observationNoiseM, observationNoiseN);
+
+
+	for (int m = 0; m < observationNoiseM; m++)
+	{
+		for (int n = 0; n < observationNoiseN; n++)
+		{
+			observationNoiseCov(m, n) = observationNoisePTR[m + observationNoiseM*n];
+		}
+	}
+
+	//Get the initial_state
+	double *initialStatePTR = mxGetPr(prhs[5]);
+	int initialStateM = mxGetM(prhs[5]);
+	int initialStateN = mxGetN(prhs[5]);
+
+	if (initialStateM > 1 && initialStateN > 1)
+	{
+		std::cerr << "Expecting the state vector but received a matrix" << std::endl;
+		exit(-1);
+	}
+
+	int dimsState = std::max(initialStateM, initialStateN);
+
+	VectorXd initial_state(dimsState);
+
+	if(initialStateN >= initialStateM)
+	{
+		for (int n = 0; n < initialStateN; n++)
+		{
+			observationNoiseCov(n) = observationNoisePTR[0 + observationNoiseM*n];
+		}
+	}
+	else {
+
+		for (int m = 0; m < initialStateM; m++)
+		{
+			observationNoiseCov(m) = observationNoisePTR[m + observationNoiseM*0];
+		}
+
+	}
 
 
     
@@ -69,6 +150,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     // Get the class instance pointer from the second input
     KalmanFilter *KalmanFilter_instance = convertMat2Ptr<KalmanFilter>(prhs[1]);
 
+	/*
     if(!strcmp("updateDeltaT", cmd))
 	{
 	  if(nrhs != 3)
@@ -77,149 +159,9 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 	  deltaT = *mxGetPr(prhs[2]);
 	  KalmanFilter_instance->updateDeltaT(deltaT);
 	  return;
-	}
+	}*/
 
-    if(!strcmp("associateData", cmd))
-	{
-	  if(nrhs != 7)
-	    mexErrMsgTxt("Test: Unexpected arguments.");
-	  double *matx;
-	  int M;
-	  int N;
-	  M = mxGetM(prhs[2]);
-	  N = mxGetN(prhs[2]);
-	  matx = mxGetPr(prhs[2]);
-		
-	  int Mrects = mxGetM(prhs[3]);
-	  int Nrects = mxGetN(prhs[3]);
-	  double *rects = mxGetPr(prhs[3]);
-
-	  int Mcolors = mxGetM(prhs[4]);
-	  int Ncolors = mxGetN(prhs[4]);
-	  double *colorFeatures = mxGetPr(prhs[4]);
-      
-	  int Mmeans = mxGetM(prhs[5]);
-	  int Nmeans = mxGetN(prhs[5]);
-	  double *means = mxGetPr(prhs[5]);
-      
-      size_t Kcov = mxGetNumberOfDimensions(prhs[6]);
-      const mwSize *Ncov = mxGetDimensions(prhs[6]);
-      double *covs = mxGetPr(prhs[6]);
-      
-      //Code to extract means...
-      std::vector<cv::Mat> meansArray;
-      
-      for(int nMeans = 0; nMeans < Nmeans; nMeans++)
-      {
-        double ux = means[0+Mmeans*nMeans];
-        double uy = means[1+Mmeans*nMeans];
-        
-        cv::Mat meanDiscreteError = (cv::Mat_<double>(2, 1) << ux, uy);
-        meansArray.push_back(meanDiscreteError);
-      }
-      //........................
-      
-
-      
-      //Convert the 3D matrix to a vector of covariance Mats
-      
-      std::vector<cv::Mat> covMatrices;
-      
-      for(int n2 = 0; n2 < Ncov[2]; n2++)
-      {
-          double a00 = covs[0+Ncov[0]*(0+Ncov[1]*n2)];
-          double a01 = covs[0+Ncov[0]*(1+Ncov[1]*n2)];
-          double a10 = covs[1+Ncov[0]*(0+Ncov[1]*n2)];
-          double a11 = covs[1+Ncov[0]*(1+Ncov[1]*n2)];
-          
-          cv::Mat covMatrix = (cv::Mat_<double>(2, 2) << a00, a01, a10, a11);
-          covMatrices.push_back(covMatrix);
-      }
-
-	  //Convert the points matrix to a std::vector
-	  std::vector<cv::Point3d> basePoints;
-	  
-	  for(int n = 0; n<N; n++)
-	    {
-	      cv::Point3d point;
-	      point.x = matx[0+M*n];
-	      point.y = matx[1+M*n];
-	      point.z = matx[2+M*n];
-	      basePoints.push_back(point);
-	    }
-	  
-	  //Convert the rectangles to a list of cv::Rect_
-	  std::vector<cv::Rect_<int> > bboxes;
-	  for(int m = 0; m < Mrects; m++)
-	    {
-	      int topLeftX;
-	      int topLeftY;
-	      int width;
-	      int height;
-
-	      topLeftX = rects[m+Mrects*0]; //first column
-	      topLeftY = rects[m+Mrects*1]; //second column
-	      width = rects[m+Mrects*2]; //third column
-	      height = rects[m+Mrects*3]; //fourth column
-
-	      cv::Rect_<int> bbox(topLeftX, topLeftY, width, height);
-	      bboxes.push_back(bbox);
-	    }
-
-	  //Color features
-	  std::vector<cv::Mat> colorFeaturesList;
-	  for(int m = 0; m < Mcolors; m++) //For each row -> each histogram
-	    {
-	      cv::Mat bvtHist = cv::Mat::zeros(1, Ncolors, CV_32F);;
-	      for(int n = 0; n < Ncolors; n++) // Fill a mat
-	        {
-		  bvtHist.at<float>(0,n) = colorFeatures[m+Mcolors*n];
-		}
-	    
-	      colorFeaturesList.push_back(bvtHist);
-	    }
-
-	  PersonList_instance->associateData(basePoints, bboxes, colorFeaturesList, meansArray, covMatrices);
-
-	  //Kill tracklets that need to be killed
-	  PersonList_instance->trackletKiller();
-
-	  int numTracklets = PersonList_instance->personList.size();
-
-	  //Return a matrix containing the 3D head points
-
-
-	  // [x, y z, id] -> each tracklet a row
-	  plhs[0] = mxCreateDoubleMatrix(numTracklets, 4, mxREAL);
-	  
-	  // each tracklet a row
-          plhs[1] = mxCreateDoubleMatrix(numTracklets, 3, mxREAL);	  
-
-	  double *trackingPoints;
-	  double *probabilities;
-
-	  trackingPoints = mxGetPr(plhs[0]);
-	  probabilities = mxGetPr(plhs[1]);
-	  
-	  int mOut = 0;
-	  
-	  for(vector<PersonModel>::iterator it = PersonList_instance->personList.begin(); it != PersonList_instance->personList.end(); it++, mOut++)
-            {
-		Point3d head = it->getPositionEstimate();
-		trackingPoints[mOut + numTracklets*0] = head.x;
-		trackingPoints[mOut + numTracklets*1] = head.y;
-		trackingPoints[mOut + numTracklets*2] = head.z;
-		trackingPoints[mOut + numTracklets*3] = it->id;
-
-		int nOut = 0;
-		for(std::vector<double>::iterator pr = it->mmaeEstimator->probabilities.begin(); pr != it->mmaeEstimator->probabilities.end(); pr++, nOut++)
-                  {
-		    probabilities[mOut+numTracklets*nOut] = *pr;
-		  }
-	    }
-
-	  return;
-	}
+ 
     
     // Got here, so command not recognized
     mexErrMsgTxt("Command not recognized.");
